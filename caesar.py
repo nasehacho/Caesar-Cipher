3a66047e70ae7a49c19a83e88755059ffee94c85
Naseha <nasehacho@gmail.com>
2024-01-09 21:44:04 -0500

    added caesar.py

---
+++ caesar.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def encrypt(message, k):
+    return
+
+
+def decrypt(message, k):
+    return
+
+
+if __name__ == "__main__":
+    # take in first arg as word
+    message = sys.argv[1]
+    # take in second arg as int key
+    key = int(sys.argv[2])
+
+    # encrypt your word
+    encrypted = encrypt(message, key)
+
+    # decrypt your encrypted word
+    decrypted = decrypt(encrypted, key)
+
+    print("Your encrypted word is", encrypted)
+    print("Your decrypted word is", decrypted)
