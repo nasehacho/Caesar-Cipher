bd78b2e3089a4ce33e5660ece1226640f5038d08
Naseha <nasehacho@gmail.com>
2024-01-29 23:10:44 -0500

    Completed Cipher

--- caesar.py
+++ caesar.py
@@ -1,5 +1,5 @@
 # define message variable and key value variable 
-message = "SECRET"
+message = "Secret"
 k = 10 
 
 # defining function "encrypt" and its arguments "message" and "k"
@@ -13,10 +13,15 @@ def encrypt(message, k):
         # implement the caesar cipher by adding our key value, 10 to the char__num we have so far
         shift_code = char_num + k 
         # to make sure the shifting restarts when we hit the letter "z", implement and "if statement" to only return alphabetic characters
-        # after 90 on the ASCII table, there are no uppercase alphabetic letters therefore, numbers after 90 on the table should be changed
-        if shift_code > 90:
-            # there are 26 letters in the alphabet, once we hit "z" subtracting by 26 will bring us back to "A"
-            shift_code = shift_code - 26 
+        if x.islower():
+        # after "z" on the ASCII table, there are no lowercase alphabetic letters therefore, numbers after "z" on the table should be changed
+            if shift_code > ord('z'):
+            # there are 26 letters in the alphabet, once we hit "z" subtracting by 26 will bring us back to "a"
+                shift_code = shift_code - 26 
+        # "elif statement" added to the condition to accomodate for upper case letters if there are no lower case letters
+        elif x.isupper():
+            if shift_code > ord('Z'):
+                shift_code = shift_code - 26 
         # convert the number code back into alphabetic characters using the chr() function
         caesar_code = chr(shift_code)
         # add only caesar_code function onto the message to only bring back our encrypted text
@@ -32,9 +37,9 @@ def decrypt(encrypted, k):
         char_num = ord(x)
         # we want to "unshift" our code to bring back the original message, therefore, we subtract from the key value 
         shift_code = char_num - k 
-        # to incorporate only alphabetic letters again, we add an "if statement" to only incorporate uppercase letters of the ASCII table 
-        if shift_code < 65:
-            # add 26 instead of subtract because we want to keep it in the upper case alphabet
+        # to incorporate only alphabetic letters again, we add an "if statement" to only incorporate letters of the ASCII table 
+        if shift_code < ord('a'):
+             # add 26 instead of subtract because we want to keep it in the lower case alphabet
             shift_code = shift_code + 26 
         caesar_code = chr(shift_code)
         decrypted = decrypted + caesar_code
